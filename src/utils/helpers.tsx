17b5d0bd1d6f0cab1aa904f1fbbe92145e09bdb0
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2024-11-24 02:27:37 +0530

    24th Nov 24 Commit

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -13,6 +13,6 @@ export const resturantStockImages = [
   "https://raw.githubusercontent.com/CVSCharan/resturant-app-assets/refs/heads/main/restaurant-stock-img-2.jpg",
   // "https://raw.githubusercontent.com/CVSCharan/resturant-app-assets/refs/heads/main/restaurant-stock-img-3.jpg",
   "https://raw.githubusercontent.com/CVSCharan/resturant-app-assets/refs/heads/main/restaurant-stock-img-4.jpg",
-  "https://raw.githubusercontent.com/CVSCharan/resturant-app-assets/refs/heads/main/restaurant-stock-img-5.jpg",
+  // "https://raw.githubusercontent.com/CVSCharan/resturant-app-assets/refs/heads/main/restaurant-stock-img-5.jpg",
   "https://raw.githubusercontent.com/CVSCharan/resturant-app-assets/refs/heads/main/restaurant-stock-img-6.jpg",
 ];

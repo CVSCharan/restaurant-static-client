bb64f52291133c7ca96b1bf33cbf727e2b9737c7
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2024-11-29 01:57:16 +0530

    add loader on landing

--- src/components/LoaderModal.tsx
+++ src/components/LoaderModal.tsx
@@ -2,17 +2,6 @@ import * as React from "react";
 import Modal from "@mui/material/Modal";
 import loaderModalStyles from "../styles/LoaderMoadl.module.css";
 
-const style = {
-  position: "absolute",
-  transform: "translate(-50%, -50%)",
-  width: "100%",
-  height: "100%",
-  bgcolor: "transparent",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
 // Define the types for the props
 interface LoaderModalProps {
   open: boolean; // Whether the modal is open
